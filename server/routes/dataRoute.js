4babe2c9ddf8314a1d60d238202337dd3d77a11e
shailiran <shailiran@gmail.com>
2021-05-08 22:32:36 +0300

    finish

--- server/routes/dataRoute.js
+++ server/routes/dataRoute.js
@@ -4,12 +4,16 @@ const Weather = require('../modles/Weather');
 const dataRouter = express.Router();
 dataRouter.get('/', async (req, res) => {
   try {
-    var lat = req.body.lat;
-    var lon = req.body.lon;
+    const { lat, lon } = req.query;
     var data = await Weather.find({ Longitude: lon, Latitude: lat });
-    res.json(data);
+    res.json(data.map(datum => ({
+      ForecastTime: datum.forecast_time, 
+      Temperature: datum.Temperature_Celsius, 
+      Precipitation: datum.Precipitation_Rate
+    })));
   } catch (error) {
-    res.json({ message: error });
+    console.log(error);
+    res.send('Error:' + error);
   }
 });
 
