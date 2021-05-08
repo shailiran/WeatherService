cb557935c2a91b5755231c056b65332eb4adc253
shailiran <shailiran@gmail.com>
2021-05-08 09:34:25 +0300

    connect to db

--- app.js
+++ app.js
@@ -1,50 +1,20 @@
+require('dotenv/config');
+
 var express = require('express');
 var app = express();
 app.listen(3000, () => {
   console.log('Server listening on 3000');
 })
 
+// Import Routes
+const dataRoute = require('./server/routes/dataRoute');
+const summarizeRoute = require('./server/routes/summarizeRoute');
+
+app.use('/data', dataRoute);
+app.use('/summarize', summarizeRoute);
+
 const mongoose = require('mongoose');
-mongoose.connect('URL', () => {
-  console.log('Connected to Mongo DB Successfully');
-})
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-
-// // Connection URL
-// const url = 'mongodb://localhost:27017';
-
-// // Database Name
-// const dbName = 'myproject';
-// const client = new MongoClient(url, {useNewUrlParser: true});
-
-// // Use connect method to connect to the server
-// client.connect(function(err) {
-//   assert.equal(null, err);
-//   console.log("Connected successfully to server");
-
-//   const db = client.db(dbName);
-
-//   insertDocuments(db, function() {
-//     client.close();
-//   });
-// });
-
-// const insertDocuments = function(db, callback) {
-//   // Get the documents collection
-//   const collection = db.collection('documents');
-//   // Insert some documents
-//   collection.insertMany([
-//     {a : 1}, {a : 2}, {a : 3}
-//   ], function(err, result) {
-//     assert.equal(err, null);
-//     assert.equal(3, result.result.n);
-//     assert.equal(3, result.ops.length);
-//     console.log("Inserted 3 documents into the collection");
-//     callback(result);
-//   });
-// }
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('Successfully connected to Mongodb!');
+});
 
