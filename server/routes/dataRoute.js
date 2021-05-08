f995b241e1696f78f776626d6785b3664ec446af
shailiran <shailiran@gmail.com>
2021-05-08 12:50:42 +0300

    find data

--- server/routes/dataRoute.js
+++ server/routes/dataRoute.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const Weather = require('../modles/Weather');
 
 const dataRouter = express.Router();
-dataRouter.get('/', (req, res) => {
-    res.send('DATA');
+dataRouter.get('/', async (req, res) => {
+  try {
+    var lat = req.body.lat;
+    var lon = req.body.lon;
+    var data = await Weather.find({ Longitude: lon, Latitude: lat });
+    res.json(data);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = dataRouter;
-
