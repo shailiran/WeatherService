8c88091194678e5c0acade6837b7d9f921f9f05d
shailiran <shailiran@gmail.com>
2021-05-08 12:50:07 +0300

    add notes

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.listen(3000, () => {
 // Import Routes
 const dataRoute = require('./server/routes/dataRoute');
 const summarizeRoute = require('./server/routes/summarizeRoute');
-
 app.use('/data', dataRoute);
 app.use('/summarize', summarizeRoute);
 
+// Connect to db
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log('Successfully connected to Mongodb!');
